4a5f74229beddc4479c251e0d1ce205cbff03835
Cesar <139836008+cesarf95@users.noreply.github.com>
2025-10-14 22:26:16 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,102 @@
+/*let nome = 'Cesar'
+let idade = 29
+let cidade = 'Maringa'
+
+console.log(`Sou ${nome}, tenho ${idade} é moro em ${cidade} `);
+*/
+
+/*let altura = 10
+let largura = 20
+let area = altura * largura;
+
+console.log("Altura do retangulo:", altura);
+console.log("Largura do retangulo:", largura);
+console.log("Area do retangulo:", area);
+*/
+
+/*const celsius = 35.0
+
+const fahrenheit = (celsius * 9/5) + 32;
+
+const kelvin = celsius + 273.15;
+
+console.log(`${celsius}°C é igual a ${fahrenheit}°F`);
+console.log(`${celsius}°C é igual a ${kelvin}K`);
+*/
+/*
+let saldo = 100;
+ saldo += 20;
+ saldo -= 30;
+ saldo -=  4.50 ;
+
+console.log(` O Saldo final é de:  ${saldo}`);
+*/
+/*
+const preco = 150;
+const descontoPorcent = 10;
+const valorDesconto = preco * (1 - descontoPorcent / 100)
+const descontotal = preco - valorDesconto   
+console.log(`O Preço final com desconto é : ${valorDesconto}`)
+console.log(`o Desconto foi de ${descontotal} Reais`)
+*/
+
+/*
+let n = 13
+if (n % 2 ===0) {
+    console.log(`o Numero ${n} e par`)
+}
+else{
+    console.log(`O numero ${n} e impar`)
+}
+*/
+/*
+let nota1 = 1
+let nota2 = 2
+let nota3 = 4
+let media = [nota1 + nota2 + nota3 / 2]
+console.log(`A media do aluno é: ${media}`)
+if (media >= 7) {
+    console.log(`Aluno aprovado`)
+}
+else {
+    (media < 5)
+    console.log(`Em Recuperacao`)
+}
+if (media < 4) {
+    console.log(`Reprovado`)
+}
+*/
+/*
+let renda = 100000
+if (renda <= 2000) {
+    console.log(`Isento`)
+}
+else if (renda >= 2000 && renda < 5000){
+    console.log(`Imposto de 5%`)
+}
+else if (renda >= 5000 && renda < 10000){
+    console.log(`Imposto de 10%`)
+}
+else{
+    (renda > 10000)
+    console.log(`Imposto de 15%`)
+}
+*/
+
+let horario = 00
+
+if (horario >= 00 && horario < 23) {
+    console.log(`Boa noite`)
+}
+
+else if (horario >= 6 && horario <11) {
+    console.log(`Bom dia`)
+}
+
+else{
+    (horario >= 12 &&  horario < 23) 
+    console.log(`Boa tarde`)
+}
+
+
+
